f91e7a701278948f55a1d8994dfa904c12e9f071
feizheng <1290657123@qq.com>
2016-11-21 13:19:14 +0800

    update ostype for mysql

--- index.sh
+++ index.sh
@@ -3,7 +3,12 @@
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
 # Usage: mysql.server  {start|stop|restart|reload|force-reload|status}  [ MySQL server options ]
-alias mysql-start='mysql.server start';
-alias mysql-stop='mysql.server stop';
-alias mysql-reload='mysql.server reload';
-alias mysql-login='mysql -uroot -p';
+if [[ $USM_OS_TYPE='macos' ]]; then
+  echo 'mac mysql...';
+  alias mysql-start='mysql.server start';
+  alias mysql-stop='mysql.server stop';
+  alias mysql-reload='mysql.server reload';
+  alias mysql-login='mysql -uroot -p';
+else
+
+fi
