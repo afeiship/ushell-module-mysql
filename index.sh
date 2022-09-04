279698d0fce9735448cf885c2762c0cf417b9d00
afeiship <1290657123@qq.com>
2022-09-04 10:48:44 +0800

    feat: add mysql-client

--- index.sh
+++ index.sh
@@ -2,33 +2,29 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
+# load modules
 source $ROOT_PATH/functions.sh;
+source $ROOT_PATH/modules/mysql-client.sh;
 
 # Usage: mysql.server  {start|stop|restart|reload|force-reload|status}  [ MySQL server options ]
 if [[ $USM_OS_TYPE==macos ]]; then
-  alias mysql-start='mysql.server start';
-  alias mysql-stop='mysql.server stop';
-  alias mysql-reload='mysql.server reload';
-  alias mysql-login='mysql -uroot -p';
+    alias mysql-start='mysql.server start';
+    alias mysql-stop='mysql.server stop';
+    alias mysql-reload='mysql.server reload';
+    alias mysql-login='mysql -uroot -p';
 fi
 
 if [[ $USM_OS_TYPE==centos ]]; then
-  alias mysql-start='service mariadb start';
-  alias mysql-stop='service mariadb stop';
-  alias mysql-status='service mariadb status';
-  alias mysql-restart='service mariadb restart';
-  alias mysql-resetpwd='mysql_secure_installation';
-  alias mysql-data='cd /var/lib/mysql';
-  alias mysql-login='mysql -u root -p';
-  alias mysql-update-to-remote='usm_mysql_update_remote';
-  alias mysql-update-password='usm_mysql_update_password';
+    alias mysql-start='service mariadb start';
+    alias mysql-stop='service mariadb stop';
+    alias mysql-status='service mariadb status';
+    alias mysql-restart='service mariadb restart';
+    alias mysql-resetpwd='mysql_secure_installation';
+    alias mysql-data='cd /var/lib/mysql';
+    alias mysql-login='mysql -u root -p';
+    alias mysql-update-to-remote='usm_mysql_update_remote';
+    alias mysql-update-password='usm_mysql_update_password';
 fi
 
-
-# brew install mysql@5.7
-export PATH="/usr/local/opt/mysql@5.7/bin:$PATH";
-export LDFLAGS="-L/usr/local/opt/mysql@5.7/lib"
-export CPPFLAGS="-I/usr/local/opt/mysql@5.7/include"
-export PKG_CONFIG_PATH="/usr/local/opt/mysql@5.7/lib/pkgconfig"
 
 unset ROOT_PATH;
---
+++ modules/mysql-client.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+export PATH="/usr/local/opt/mysql-client/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/mysql-client/lib"
+export CPPFLAGS="-I/usr/local/opt/mysql-client/include"
